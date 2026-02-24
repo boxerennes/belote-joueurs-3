10d38ae8a27e2a4ca59832d33a31da480f90c2f0
boxerennes <cganimateur@gmail.com>
2026-02-24 20:21:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
-// Service Worker pour permettre l'installation
-self.addEventListener('fetch', function(event) {});
+// Service Worker minimal pour permettre l'installation PWA
+self.addEventListener('fetch', function(event) {
+    // Ce code permet à l'application de fonctionner même avec une connexion instable
+});
